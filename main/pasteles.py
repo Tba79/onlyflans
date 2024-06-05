f2d30f2b61561f1f1a0d5a5bf7d49ef28a0e3e35
Esteban Vergara <estvergara@gmail.com>
2024-06-05 05:38:09 -0400

    comit

---
+++ main/pasteles.py
@@ -0,0 +1,30 @@
+pasteles = [
+    {
+    'id': 1,
+        'nombre': 'Flan de café',
+        'descripcion': 'El flan de café es perfecto para los amantes de esta bebida.',
+        'imagen_url': 'https://imag.bonviveur.com/flan-de-cafe.webp',
+        'precio': 3500,
+    },
+    {
+        'id': 2,
+        'nombre': 'Flan de María',
+        'descripcion': 'Flan con trozos de galletas clasicas de galletas María.',
+        'imagen_url': 'https://imag.bonviveur.com/presentacion-principal-del-flan-de-galletas-maria.webp',
+        'precio': 2500,
+    },
+    {
+        'id': 3,
+        'nombre': 'Bizcoflan',
+        'descripcion': 'Bizcho y flan de caramelo, una mezcla perfecta',
+        'imagen_url': 'https://imag.bonviveur.com/bizcoflan.webp',
+        'precio': 3000,
+    },
+    {
+        'id': 4,
+        'nombre': 'Flan de chocolate',
+        'descripcion': 'Flan irresistible, de textura sedosa y con un intenso sabor a chocolate.',
+        'imagen_url': 'https://imag.bonviveur.com/flan-de-chocolate.webp',
+        'precio': 3500,
+    },
+]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from main.pasteles import pasteles
 
 # Create your views here.
 def index (request):
-    return render(request, 'index.html')
+    context = {'pasteles': pasteles}
+    return render(request, 'index.html', context)
+    
 
 def about (request):
     return render(request, 'about.html')
 
 def welcome (request):
     return render(request, 'welcome.html')
+
+
