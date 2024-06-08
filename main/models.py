f449f6db106459b124ccdfed24d8b46eeb5925a4
Esteban Vergara <estvergara@gmail.com>
2024-06-08 13:37:28 -0400

    [ADD/CREATE] Modificaion en archvis VIEWS, creacion de vinculacion de BASE DE DATOS

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    nombre = models.CharField(max_length = 64)
+    email = models.CharField(max_length = 64)
+    mensaje = models.CharField(max_length = 100)
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from main.pasteles import pasteles
 from main.forms import ContactForm
+from main.models import Contact
 
 # Create your views here.
 def index (request):
@@ -21,6 +22,9 @@ def welcome(request):
     else:
         form = ContactForm(request.POST) # Se crea una instancia de ContactForm con los datos enviados en la solicitud POST.
         if form.is_valid(): # Se verifica si los datos del formulario son válidos.
+            Contact.objects.create(
+                **form.cleaned_data
+            )
             return redirect('/contactcheck') # Si el formulario es válido, se redirige al usuario a la URL '/success'.
         context = {'form': form} # Se crea un contexto que contiene el formulario con los datos (válidos o no).
         return render(request, 'welcome.html', context) # Se vuelve a renderizar la plantilla con el contexto actualizado.
@@ -30,26 +34,7 @@ def contactcheck (request):
     return render(request, 'contactcheck.html')
 
 
-# def contact_form (request):
-#     errores = []
-    
-#     customer_name = request.POST['customer_name']
-#     customer_email = request.POST['customer_email']
-#     message = request.POST['message']
-    
-#     if len(customer_name) > 6:
-#         errores.append('Largo mayor a 6')
-        
-#     if  not '@' in customer_email:
-#         errores.append('Mail no valido, falta tu arroba @ de BB')
 
-#     context = {'errores': errores}
-#     if len(errores) > 0:
-#         return render(request,'welcome.html', context)
-#     else:
-        
-#         return render(request, 'contactcheck.html', context)
-    
 
 def ayuda (request):
     return render(request, 'ayuda.html')
