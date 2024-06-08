03c5a3b9da0fe88ad7a4770b750f63b3f51fcb14
Esteban Vergara <estvergara@gmail.com>
2024-06-08 14:01:45 -0400

    Modificacion de archivo

--- main/admin.py
+++ main/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from main.models import Contact
 
 # Register your models here.
+class PersonaAdmin(admin.ModelAdmin):
+    pass
 
-admin.site.register(Contact)
+admin.site.register(Contact, PersonaAdmin)
--- main/forms.py
+++ main/forms.py
@@ -3,26 +3,26 @@ from django import forms
 class ContactForm(forms.Form):
     nombre = forms.CharField(
         max_length=10,
-        label='Nombre:',
+        label='Nombre',
         widget=forms.TextInput(attrs={
             'class': 'form-control mb-3',
-            'placeholder': 'Ingrese nombre aquí'
+            'placeholder': 'Ejemplo: Juana FlanLover'
         })
     )
     email = forms.EmailField(
         max_length=20,
-        label='Email:',
+        label='Correo Electronico',
         widget=forms.EmailInput(attrs={
             'class': 'form-control mb-3',
-            'placeholder': 'Ingrese correo aquí'
+            'placeholder': 'Ejemplo: flanlover@flan.com'
         })
     )
     mensaje = forms.CharField(
-        label='Mensaje:',
+        label='Mensaje',
         widget=forms.Textarea(attrs={
             'class': 'form-control mb-3',
             'rows': 5,
-            'placeholder': 'Ingrese mensaje'
+            'placeholder': 'Ingrese su mensaje'
         })
     )
 
